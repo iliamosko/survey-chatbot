784ccf3076966fb3a3eeb35e18c559de412e799d
Ilia <iliamosko@gmail.com>
2019-03-19 19:00:32 -0400

    Added pauses in between bot chat messages

--- src/main/resources/static/chatHandler.js
+++ src/main/resources/static/chatHandler.js
@@ -1,111 +1,119 @@
-var globVal = 0;
+//Connecting function, waits for a "connect" then continues with code
+function connecting() {
+   // waitingDots();
+    var str = "<b>Please wait, You will be connected Shortly</b>";
+    document.getElementById("inputBox").disabled = true;
+    document.getElementById("inputBox")
+        .placeholder = " ";
 
-
-
-
-//this runs after the /api/chat is successful and returns the bots answer
-function onSuccess(tmp){
-    console.log(tmp);
-
-    var node = document.createElement("P");
-    var textNode = document.createTextNode(tmp.output.text);
-    node.appendChild(textNode);
-    node.className = "chat-message";
-    document.getElementById("log").appendChild(node);
-    //globVal++;
-    //console.log(globVal);
-    scrollBottom();
-
-}
-
-//at the start of the page, this is executed to display "hello" in the chatlog div
-function onStart(){
-
-
+    document.getElementById('con').innerHTML = str;
     setTimeout(function () {
-        var node = document.createElement("P");
-        var textNode = document.createTextNode("Hello");
-        node.appendChild(textNode);
-        node.className = "chat-message";
-        document.getElementById("log").appendChild(node);
-    //    globVal++;
-    },2000);
-    maxLength(document.getElementById("inputBox"));
+        document.getElementById('con').innerHTML = '';
+        document.getElementById("inputBox")
+            .placeholder = "Type Here";
+        document.getElementById("inputBox").disabled = false;
+        onStart();
+    }, 5000);
 }
 
 //when the send button is pressed, this gets executed
-function submit(){
-
+function submit() {
     var tmp;
     var element = document.getElementById("inputBox");
 
     console.log(element);
     if (element != null) {
         tmp = element.value;
-    }
-    else {
+    } else {
         tmp = null;
     }
 
-     var node = document.createElement("P");
-     var textNode = document.createTextNode(tmp);
-     node.appendChild(textNode);
-     node.className = "chat-messageU";
-     document.getElementById("log").appendChild(node);
-     //globVal++;
+    var node = document.createElement("div");
+    var textNode = document.createTextNode(tmp);
+    node.appendChild(textNode);
+    node.className = "chat-messageU";
+    document.getElementById("log").appendChild(node);
 
     console.log(tmp);
     document.getElementById("inputBox").value = "";
 
     scrollBottom();
 
-
     //Jquery call to the watson API
     $.ajax({
-      type: "POST",
-      url: "/api/chat",
-      data: {msg:tmp},
-      success: onSuccess,
-      dataType: "json"
+        type: "POST",
+        url: "/api/chat",
+        data: {msg: tmp},
+        success: onSuccess,
+        dataType: "json"
     });
 
 }
+
+//this runs after the /api/chat is successful and returns the bots answer
+function onSuccess(tmp) {
+    console.log(tmp);
+    var node = document.createElement("div");
+    var textNode = document.createTextNode(tmp.output.text);
+    node.appendChild(textNode);
+    node.className = "chat-message";
+
+    if (textNode.length > 10) {
+        setTimeout(function () {
+            document.getElementById("log").appendChild(node);
+            scrollBottom();
+        }, 3000);
+    } else {
+        setTimeout(function () {
+            document.getElementById("log").appendChild(node);
+            scrollBottom();
+        }, 1000);
+    }
+}
+
+//After connecting function is finished, this is executed to display "hello" in the chatlog div
+function onStart() {
+    setTimeout(function () {
+        var node = document.createElement("div");
+        var textNode = document.createTextNode("Hello");
+        node.appendChild(textNode);
+        node.className = "chat-message";
+        document.getElementById("log").appendChild(node);
+        //    globVal++;
+    }, 3000);
+    maxLength(document.getElementById("inputBox"));
+}
+
 //checks when the user presses enter to send a message
-function enterPress(e, textarea){
+function enterPress(e, textarea) {
     var code = (e.keyCode ? e.keyCode : e.which);
-    if(code == 13) { //Enter keycode
+    if (code == 13) { //Enter keycode
         submit();
     }
 }
+
 //sets the max length of the textarea
 function maxLength(el) {
-	if (!('maxLength' in el)) {
-		var max = el.attributes.maxLength.value;
-		el.onkeypress = function () {
-			if (this.value.length >= max) return false;
-		};
-	}
+    if (!('maxLength' in el)) {
+        var max = el.attributes.maxLength.value;
+        el.onkeypress = function () {
+            if (this.value.length >= max) return false;
+        };
+    }
+}
+//clear interval could work in anyother function.... just need to put clearn interval
+function waitingDots(){
+    var dots = window.setInterval( function() {
+        var wait = document.getElementById("dots");
+        if ( wait.innerHTML.length > 3 )
+            wait.innerHTML = "";
+        else
+            wait.innerHTML += ".";
+    }, 1000);
+
 }
 
 
-//!!!!!!!!!!!! STILL NEEDS WORK
-function connecting(){
-    var tmp = "you are getting connected with a user";
-
-     var node = document.createElement("span");
-     node.className = "waiting-Dots";
-     document.getElementById("log").appendChild(node);
-
-     var span = document.getElementByClassName("waiting-Dots");
-
-     var int = setInterval(function() {
-         if ((span.innerHTML += '.').length == 4)
-             span.innerHTML = '';
-         //clearInterval( int ); // at some point, clear the setInterval
-     }, 1000);
-
-}
-
-function scrollBottom(){
-    document.querySelector('#log').scrollTop = document.querySelector('#log').scrollHeight
+function scrollBottom() {
+    document.querySelector('#log').scrollTop = document.querySelector('#log').scrollHeight;
 }
